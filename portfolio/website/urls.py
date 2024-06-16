d87c2e89ca706b288ae6082dfa2b975b5cc2248f
Pankaj Basnet <pankajbasnet2020@hotmail.com>
2024-06-16 23:07:06 +0545

    created navbar.html----extended to base.html

--- portfolio/website/urls.py
+++ portfolio/website/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("", home, name = "home"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
